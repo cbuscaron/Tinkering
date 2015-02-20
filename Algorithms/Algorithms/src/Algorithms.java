b952cf7842e5a4be8fdbcc9aeb518c3d9752051b
cbuscaron <cbuscaron@gmail.com>
2015-02-19 22:42:13 -0500

    Java Algorithms

---
+++ Algorithms/Algorithms/src/Algorithms.java
@@ -0,0 +1,12 @@
+
+public class Algorithms {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello Pepe");
+		System.out.println("Ping");
+	}
+
+}
+
